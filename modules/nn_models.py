12fe91a96c4da14120e07fffcef24578e8a4873c
juanplanetai <jesteban.agudelo@planetai.space>
2025-03-04 10:48:43 +0100

    improved nn

--- modules/nn_models.py
+++ modules/nn_models.py
@@ -1,21 +1,31 @@
 import torch
 from torch import nn
 
+import torch
+from torch import nn
+
 class LinearJarolim(nn.Module):
     def __init__(self, in_shape: int, out_shape: int, hidden_units: int, n_layers: int):
         super().__init__()
         self.name = "SimpleLinear"
         self.n_layers = n_layers
-        self.input = nn.Linear(in_features = in_shape, out_features = hidden_units)
+        self.input = nn.Linear(in_features=in_shape, out_features=hidden_units)
         
-        self.hidden_linear = nn.Linear(in_features = hidden_units, out_features = hidden_units)
+        self.hidden_layers = nn.ModuleList()
+        for _ in range(n_layers):
+            self.hidden_layers.append(nn.Linear(in_features=hidden_units, out_features=hidden_units))
+            self.hidden_layers.append(nn.BatchNorm1d(hidden_units))
+            self.hidden_layers.append(nn.Dropout(p=0.5))
 
-        self.output = nn.Linear(in_features = hidden_units, out_features = out_shape)
+        self.output = nn.Linear(in_features=hidden_units, out_features=out_shape)
         
     def forward(self, x):
         x = self.input(x)
-        for i in range(self.n_layers):
-            x = self.hidden_linear(x)
-            x = torch.sin(x)
+        for layer in self.hidden_layers:
+            if isinstance(layer, nn.Linear):
+                x = layer(x)
+                x = torch.sin(x)
+            else:
+                x = layer(x)
         x = self.output(x)
         return x
